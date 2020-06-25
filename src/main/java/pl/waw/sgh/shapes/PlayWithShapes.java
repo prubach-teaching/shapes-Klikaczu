3c6e9f9bfc348951b5b4685d3ceef26d9e737b32
klikaczu <k.jaszczolt@gmail.com>
2020-06-25 21:33:12 +0200

    Shapes added

--- src/main/java/pl/waw/sgh/shapes/PlayWithShapes.java
+++ src/main/java/pl/waw/sgh/shapes/PlayWithShapes.java
@@ -22,6 +22,22 @@ public class PlayWithShapes {
         Circle c1 = new Circle(5);
         System.out.println("C1: " + c1);
         System.out.println("Surface C1: " + c1.calcSurface());
+
+        Square s1 = new Square(10);
+        System.out.println("S1: " + s1);
+        System.out.println("Surface S1: " + s1.calcSurface());
+        System.out.println("Perimeter S1: " + s1.calcSurface());
+
+        Triangle t1 = new Triangle(2,3,4,3);
+        System.out.println("T1: "  + t1;
+        System.out.println("Surface T1: " + t1.calcSurface());
+        System.out.println("Perimeter T1: " + t1.calcPerimeter());
+
+        EquilateralTriangle et1 = new EquilateralTriangle(5);
+        System.out.println("ET1: " + et1);
+        System.out.println("Surface ET1: " + et1.calcSurface());
+        System.out.println("Perimeter ET1: " + et1.calcPerimeter());
+
     }
 
 }
